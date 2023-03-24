6c14df0d0819f255dc0aa9fe8b0059fc256e7ab4
h <h>
2023-03-23 20:45:52 -0500

    Switch to throw instead of console.error in premod.js

--- premod.js
+++ premod.js
@@ -19,19 +19,17 @@ function executeModdedMethod(defaultCallback,stageMethod,t) {
 }
 
 function setModConfig(config) {
-	if (config.userId < 0 || config.userId == 2) {
-		return console.error("Bad user id, must be greater than 0 and must not equal 2");
-	} else if (config.modId < 0) {
-		return console.error("Bad modId, must be positive integer");
-	} else if (config.modName.length == 0) {
-		return console.error("Mod needs name");
-	} else {
-		PETRICHOR_CURRENT_MOD_CONFIG.userId = config.userId;
-		PETRICHOR_CURRENT_MOD_CONFIG.modId = config.modId;
-		PETRICHOR_CURRENT_MOD_CONFIG.modName = config.modName
-		PETRICHOR_CURRENT_MOD_CONFIG.addedBlockIds = [];
-		PETRICHOR_CURRENT_MOD_CONFIG.addedBlockNames = [];
-	}
+	if (config.userId < 0 || config.userId == RESERVED_INVALID_USER_ID)
+		throw `Bad author user id ${config.userId}. Must be positive and must not equal the reserved invalid id (${RESERVED_INVALID_USER_ID})`;
+	if (config.modId < 0)
+		throw `Bad mod id ${config.modId}`;
+	if (config.modName.length == 0)
+		throw "Mod needs name";
+	PETRICHOR_CURRENT_MOD_CONFIG.userId = config.userId;
+	PETRICHOR_CURRENT_MOD_CONFIG.modId = config.modId;
+	PETRICHOR_CURRENT_MOD_CONFIG.modName = config.modName
+	PETRICHOR_CURRENT_MOD_CONFIG.addedBlockIds = [];
+	PETRICHOR_CURRENT_MOD_CONFIG.addedBlockNames = [];
 }
 function newBlock(blockConfig) {
 	// {
@@ -41,19 +39,19 @@ function newBlock(blockConfig) {
 	// 		console.log(params);
 	// 	}
 	// }
-	if (PETRICHOR_CURRENT_MOD_CONFIG.userId == 2) {
-		return console.error("Cannot add new block because mod is not configured.");
-	}
-	if (PETRICHOR_CURRENT_MOD_CONFIG.addedBlockIds.indexOf(blockConfig.id) != -1) {
-		return console.error("Cannot add new block, its id was already used.");
-	}
-	if (PETRICHOR_CURRENT_MOD_CONFIG.addedBlockNames.indexOf(blockConfig.name) != -1) {
-		return console.error("Cannot add new block, its name was already used.")
-	}
+	if (PETRICHOR_CURRENT_MOD_CONFIG.userId == 2)
+		throw "Cannot add new block because mod is not configured.";
+
+	if (PETRICHOR_CURRENT_MOD_CONFIG.addedBlockIds.indexOf(blockConfig.id) != -1)
+		throw "Cannot add new block, its id was already used.";
+
+	if (PETRICHOR_CURRENT_MOD_CONFIG.addedBlockNames.indexOf(blockConfig.name) != -1)
+		throw "Cannot add new block, its name was already used."
+
 	const actualId = parseInt((-PETRICHOR_CURRENT_MOD_CONFIG.userId).toString()+PETRICHOR_CURRENT_MOD_CONFIG.modId.toString()+blockConfig.id.toString());
-	if (actualId > -100) {
-		return console.error(`Problem adding block. Its actual id ended up being ${actualId} which did not make sense.`);
-	}
+	if (actualId > -100)
+		throw `Problem adding block. Its actual id ended up being ${actualId} which did not make sense.`;
+
 	const actualName = PETRICHOR_CURRENT_MOD_CONFIG.userId.toString()+PETRICHOR_CURRENT_MOD_CONFIG.modName+blockConfig.name;
 	console.log(actualId,actualName);
 	// Add to enum
